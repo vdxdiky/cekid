867e6a616dc40a628d879732e2ace5ca5163c492
vdxdiky <dikyvdx@gmail.com>
2025-03-23 04:59:05 +0700

    Update ml.ts

--- src/router/ml.ts
+++ src/router/ml.ts
@@ -2,7 +2,7 @@ import { hitCoda, Result } from '../utils'
 
 export default async function ml(id: number, zone: number): Promise<Result> {
   try {
-    const url = `https://api.ryzendesu.vip/api/stalk/ml?userId=${id}&zoneId=${zone}`
+    const url = `https://dev.luckycat.my.id/api/stalker/mobile-legend?users=${id}&servers=${zone}`
     const headers = {
       'accept': 'application/json',
       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36', // Common user-agent header
@@ -22,17 +22,17 @@ export default async function ml(id: number, zone: number): Promise<Result> {
     console.log('API response:', data)
 
     // Check if the API response is successful
-    if (data.success !== true) {
-      throw new Error(data.message || 'Error occurred while fetching data')
+    if (data.status !== true) {
+      throw new Error(data.msg || 'Error occurred while fetching data')
     }
 
     // Extract relevant data from the response
-    const username = data.username
-    const region = data.region
+    const nickname = data.data.nickname
+    const country = data.data.country
 
-    // Check if username exists in the response
-    if (!username) {
-      throw new Error('Username is missing in the response')
+    // Check if nickname exists in the response
+    if (!nickname) {
+      throw new Error('Nickname is missing in the response')
     }
 
     return {
@@ -40,8 +40,8 @@ export default async function ml(id: number, zone: number): Promise<Result> {
       game: 'Mobile Legends: Bang Bang',
       id,
       server: zone,
-      name: username,
-      region,
+      name: nickname,
+      country,
     }
   } catch (error) {
     console.error('Error in ml function:', error)
